dad174ec0c85af17187d46048d8600fb3c3e4456
Zeeshan <zsaeed830@gmail.com>
2022-12-13 15:09:45 +0500

    Frontend & functionality completed

---
+++ public/js/main.js
@@ -0,0 +1,58 @@
+const onSubmit = (e) => {
+  e.preventDefault();
+
+  document.querySelector(".msg").textContent = "";
+  document.querySelector("#image").src = "";
+
+  const prompt = document.querySelector("#prompt").value;
+  const size = document.querySelector("#size").value;
+
+  if (prompt === "") {
+    alert("Please add some text ");
+    return;
+  }
+
+  generateImageRequest(prompt, size);
+};
+
+const generateImageRequest = async (prompt, size) => {
+  try {
+    showSpinner();
+    const response = await fetch("/openai/generateimage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+        size,
+      }),
+    });
+
+    if (!response.ok) {
+      removeSpinner();
+      throw new Error("That image could not be generated");
+    }
+
+    const data = await response.json();
+    // console.log(data);
+
+    const imgUrl = data.data;
+
+    document.querySelector("#image").src = imgUrl;
+
+    removeSpinner();
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
+};
+
+// Add spinner to screen
+const showSpinner = () =>
+  document.querySelector(".spinner").classList.add("show");
+
+// Remove spinner from  screen
+const removeSpinner = () =>
+  document.querySelector(".spinner").classList.remove("show");
+
+document.querySelector("#image-form").addEventListener("submit", onSubmit);
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const { generateImage } = require("../controllers/openaiController");
 const router = express.Router();
 
